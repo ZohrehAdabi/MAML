b995b9442765d8d0d3dbae09d1870e3e5ee1fb0c
Zohreh Adabi <adabi1458@gmail.com>
2020-02-12 10:22:51 +0330

    add eval_sine_test

---
+++ eval_sine_test.py
@@ -0,0 +1,39 @@
+eval_sine_test
+
+def eval_sine_test(model, optimizer, x, y, x_test, y_test, num_steps=(0, 1, 10)):
+    '''Evaluate how the model fits to the curve training for `fits` steps.
+    
+    Args:
+        model: Model evaluated.
+        optimizer: Optimizer to be for training.
+        x: Data used for training.
+        y: Targets used for training.
+        x_test: Data used for evaluation.
+        y_test: Targets used for evaluation.
+        num_steps: Number of steps to log.
+    '''
+    fit_res = []
+    
+    tensor_x_test, tensor_y_test = np_to_tensor((x_test, y_test))
+    
+    # If 0 in fits we log the loss before any training
+    w3_copied_model = []
+    if 0 in num_steps:
+        loss, logits = compute_loss(model, tensor_x_test, tensor_y_test)
+        fit_res.append((0, logits, loss))
+        w3_copied_model.append(model.out.get_weights()[0])
+       
+    for step in range(1, np.max(num_steps) + 1):
+        train_batch(x, y, model, optimizer)
+        loss, logits = compute_loss(model, tensor_x_test, tensor_y_test)
+        if step in num_steps:
+            fit_res.append(
+                (
+                    step, 
+                    logits,
+                    loss
+                )
+            )
+            w3_copied_model.append(model.out.get_weights()[0])
+    return fit_res, w3_copied_model
+
