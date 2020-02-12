8f592aaae38db1920c12617a9d06956c3c07a93a
Zohreh Adabi <adabi1458@gmail.com>
2020-02-12 10:25:17 +0330

    add train_maml

---
+++ train_maml.py
@@ -0,0 +1,65 @@
+train_maml
+def train_maml(model, epochs, dataset, lr_inner=0.01, batch_size=1, log_steps=1000):
+    '''Train using the MAML setup.
+    
+    The comments in this function that start with:
+        
+        Step X:
+        
+    Refer to a step described in the Algorithm 1 of the paper.
+    
+    Args:
+        model: A model.
+        epochs: Number of epochs used for training.
+        dataset: A dataset used for training.
+        lr_inner: Inner learning rate (alpha in Algorithm 1). Default value is 0.01.
+        batch_size: Batch size. Default value is 1. The paper does not specify
+            which value they use.
+        log_steps: At every `log_steps` a log message is printed.
+    
+    Returns:
+        A strong, fully-developed and trained maml.
+    '''
+    optimizer = keras.optimizers.Adam()
+    
+    # Step 2: instead of checking for convergence, we train for a number
+    # of epochs
+    for _ in range(epochs):
+        total_loss = 0
+        losses = []
+        start = time.time()
+        # Step 3 and 4
+        for i, t in enumerate(random.sample(dataset, len(dataset))):
+            x, y = np_to_tensor(t.batch())
+            model.call(x)  # run forward pass to initialize weights
+            with tf.GradientTape() as test_tape:
+                # test_tape.watch(model.trainable_variables)
+                # Step 5
+                with tf.GradientTape() as train_tape:
+                    train_loss, _ = compute_loss(model, x[0:5], y[0:5])
+                # Step 6
+                gradients = train_tape.gradient(train_loss, model.trainable_variables)
+                k = 0
+                model_copy = copy_model(model, x)
+                for j in range(len(model_copy.layers)):
+                    model_copy.layers[j].kernel = tf.subtract(model.layers[j].kernel,
+                                tf.multiply(lr_inner, gradients[k]))
+                    model_copy.layers[j].bias = tf.subtract(model.layers[j].bias,
+                                tf.multiply(lr_inner, gradients[k+1]))
+                    k += 2
+                # Step 8
+                test_loss, logits = compute_loss(model_copy, x[5:], y[5:])
+            # Step 8
+            gradients = test_tape.gradient(test_loss, model.trainable_variables)
+            optimizer.apply_gradients(zip(gradients, model.trainable_variables))
+            
+            # Logs
+            total_loss += test_loss
+            loss = total_loss / (i+1.0)
+            losses.append(loss)
+            
+            if i % log_steps == 0 and i > 0:
+                print('Step {}: loss = {}, Time to run {} steps = {}'.format(i, loss, log_steps, time.time() - start))
+                start = time.time()
+        plt.plot(losses)
+        plt.show()
