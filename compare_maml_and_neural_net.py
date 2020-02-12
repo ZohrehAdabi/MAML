29803565bdf1dbc2198e99337cc560021bcc1e94
Zohreh Adabi <adabi1458@gmail.com>
2020-02-12 10:26:45 +0330

    add compare_maml_and_neural_net

---
+++ compare_maml_and_neural_net.py
@@ -0,0 +1,44 @@
+compare_maml_and_neural_net
+def compare_maml_and_neural_net(maml, neural_net, sinusoid_generator, num_steps=list(range(10)),
+                                intermediate_plot=True, marker='x', linestyle='--'):
+    '''Compare the loss of a MAML model and a neural net.
+    
+    Fits the models for a new task (new sine wave) and then plot
+    the loss of both models along `num_steps` interactions.
+    
+    Args:
+        maml: An already trained MAML.
+        neural_net: An already trained neural net.
+        num_steps: Number of steps to be logged.
+        intermediate_plot: If True plots intermediate plots from
+            `eval_sinewave_for_test`.
+        marker: Marker used for plotting.
+        linestyle: Line style used for plotting.
+    '''
+    if intermediate_plot:
+        print('MAML')
+    fit_maml, w3_maml_model = eval_sinewave_for_test(maml, sinusoid_generator, plot=intermediate_plot)
+    if intermediate_plot:
+        print('Neural Net')
+    fit_neural_net, w3_net_model = eval_sinewave_for_test(neural_net, sinusoid_generator, plot=intermediate_plot)
+    
+    fit_res = {'MAML': fit_maml, 'Neural Net': fit_neural_net}
+    
+    legend = []
+    fig = plt.figure(figsize=(12, 5), tight_layout=True)
+    ax = fig.add_subplot(1,1,1)
+    i = 0
+    for name in fit_res:
+        x = []
+        y = []
+        for n, _, loss in fit_res[name]:
+            x.append(n)
+            y.append(loss)
+        ax.plot(x, y, marker=marker, linestyle=linestyle, color=colors[i])
+        i+=1%3
+        ax.set_xticks(num_steps)
+        ax.set_ylabel('loss')
+        legend.append(name)
+    ax.legend(legend)
+    plt.show()
+    return w3_maml_model, w3_net_model
