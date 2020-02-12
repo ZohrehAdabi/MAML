e5eb084b7351ea7d079b3f82737e3bedbe9cb857
Zohreh Adabi <adabi1458@gmail.com>
2020-02-12 10:28:43 +0330

    add save_model_weight

---
+++ save_model_weight.py
@@ -0,0 +1,6 @@
+save_model
+def save_model_weight(model, path):
+    model.save_weights(path, save_format='tf')
+
+save_model_weight(neural_net, path="./net/neural_net_model")
+save_model_weight(maml, path="./maml/maml_model")
